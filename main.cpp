1c4114ded124c7b057e799f948444ce517643bdd
Dhuang <huangdanchen@gmail.com>
2014-11-07 13:06:31 -0500

    Second Commit by Dhuang!

--- main.cpp
+++ main.cpp
@@ -6,9 +6,11 @@ using namespace std;
 int main(int argc, char **argv){
 	
    char sentence [] = "Danchen is so great!";
-
+   char quote [] = "Das ist unser land!";
+  
    printf("%s",sentence);
-
+   printf("%s",quote);
    return 0;
+
 
 }
