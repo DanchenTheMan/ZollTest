648724bad79c8e2c48ea79d3ec5f4eee5789c393
Dhuang <huangdanchen@gmail.com>
2014-11-07 12:44:17 -0500

    First Commit!

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char **argv){
+	
+   char sentence [] = "Danchen is so great!";
+
+   printf("%s",sentence);
+
+   return 0;
+
+}
